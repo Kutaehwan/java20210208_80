17a8e738be58b751e1443db8ee2ae878bc2293c7
Kutaehwan <lion0523@naver.com>
2021-02-16 16:40:18 +0900

    2월 16일 수업

---
+++ java210208/src/M2_D16/Param.java
@@ -0,0 +1,7 @@
+package M2_D16;
+
+public class Param {
+	
+	int ival;
+
+}
---
+++ java210208/src/M2_D16/ParamTest.java
@@ -0,0 +1,26 @@
+package M2_D16;
+
+public class ParamTest {
+	
+	void effectParam(Param p) {
+		// insert here일 때는 얼마가 출력되는 것일까요?
+		p = new Param();		// 인스턴스 활성화
+		System.out.println(p.ival);
+		
+		p.ival = 500;
+		System.out.println("sub ival\n" + p.ival);
+	}
+
+	public static void main(String[] args) {
+		// 클래스는 반드시 대문자 
+		ParamTest pt = new ParamTest();
+		Param p = new Param();
+		
+		p.ival = 100;
+		pt.effectParam(p);
+		//p.ival = 100;
+		System.out.println("\nmain ival\n" + p.ival);
+
+	}
+
+}
